755ac288f967509dbdf22f0aefa21b2da7837c3b
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-21 02:00:32 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, );
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, finding_goal TEXT, start_data INTEGER, end_date INTEGER);
