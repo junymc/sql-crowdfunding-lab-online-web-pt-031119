936300a6759afe9fb44c22b78c58a24274b3b364
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-21 02:55:27 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,8 +20,8 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 FROM users
 INNER JOIN pledges
 ON users.id = pledges.user_id
-GROUP BY user_id
-ORDER BY users.id;"
+GROUP BY usres.name
+ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
